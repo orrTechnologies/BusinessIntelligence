e2ff9bc2f7014788d8f4c8dcbb243de4142e4e4b
Dylan <dylan@cruisercorps.com>
2016-08-31 14:35:10 -0500

    Press enter on search box, now submits search query

--- BusinessInsights/Scripts/businessinsights.js
+++ BusinessInsights/Scripts/businessinsights.js
@@ -1,17 +1,19 @@
-﻿$(window).load(function () {
+﻿$(window).load(function() {
 
-    $("#page-search-button").click(function () {
-       var searchTerm = $('#page-search-input-box').val();
+    var submitSearch = function() {
+        var searchTerm = $('#page-search-input-box').val();
         if (searchTerm) {
             window.location = ('/facebook/search/?query=' + searchTerm);
         }
+    }
+
+    $("#page-search-button").click(function() {
+        submitSearch();
     });
-    $('#page-search-input-box input').keydown(function (e) {
+    $('#page-search-input-box').keydown(function(e) {
         if (e.keyCode == 13) {
-            $('#form').submit();
+            submitSearch();
         }
     });
 
-        Morris.Donut(donutChartParams);
-        Morris.Area(areaChartParams);
 });
